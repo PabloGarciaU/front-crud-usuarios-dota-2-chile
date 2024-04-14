17504c424f32c32762195486bf771a397142d411
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-14 01:22:45 -0400

    configurado listproduct component y agregado logo bonito
    Hasta ahora como viento en popa

--- front-crud-dota-2-chile/src/app/components/list-users/list-users.component.ts
+++ front-crud-dota-2-chile/src/app/components/list-users/list-users.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user'; // Asegúrate de importar la interfaz User desde la ubicación correcta
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrl: './list-users.component.css'
+  styleUrls: ['./list-users.component.css']
 })
-export class ListUsersComponent {
+export class ListUsersComponent implements OnInit {
+  listUsers: User[] = [
+    {
+      id: 0,
+      nombre: 'Pablo Garcia',
+      apodo: 'Canadrui',
+      rut: '20.278.438-0',
+      fechaNacimiento: new Date('1999-10-11'),
+      horasJugadas: 5503,
+      rangoActual: 'Divino'
+    },
+    {
+      id: 1,
+      nombre: 'Juan Pérez',
+      apodo: 'Juanito',
+      rut: '12.345.678-9',
+      fechaNacimiento: new Date('1990-05-15'),
+      horasJugadas: 200,
+      rangoActual: 'Avanzado'
+    },
+    {
+      id: 2,
+      nombre: 'María González',
+      apodo: 'Mary',
+      rut: '23.456.789-0',
+      fechaNacimiento: new Date('1995-12-28'),
+      horasJugadas: 150,
+      rangoActual: 'Intermedio'
+    },
+    {
+      id: 3,
+      nombre: 'Pedro Torres',
+      apodo: 'Pete',
+      rut: '34.567.890-1',
+      fechaNacimiento: new Date('1987-08-03'),
+      horasJugadas: 300,
+      rangoActual: 'Avanzado'
+    },
+    {
+      id: 4,
+      nombre: 'Ana Soto',
+      apodo: 'Anita',
+      rut: '45.678.901-2',
+      fechaNacimiento: new Date('1993-02-10'),
+      horasJugadas: 100,
+      rangoActual: 'Principiante'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
