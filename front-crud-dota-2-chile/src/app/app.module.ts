dcb182aa3d42edbf8353643c7249f397830fa128
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-14 00:40:47 -0400

    Creacion del proyecto y arquitectura del front - end
    Los datos a manejar seran los siguientes:
    
    ////	Crud Registro Usuarios Chilenos Dota 2
    ID Usuario:
    Nombre:
    Apodo:
    Rut:
    Fecha de nacimiento:
    Horas jugadas:
    Rango actual:

--- front-crud-dota-2-chile/src/app/app.module.ts
+++ front-crud-dota-2-chile/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListUsersComponent,
+    AddEditUserComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-crud-dota-2-chile/src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User{
+    id: number;
+    nombre: string;
+    apodo: string;
+    rut: string;
+    fechaNacimiento: Date;
+    horasJugadas: number;
+    rangoActual: string;
+}
