714850bf83ca10ee15be925183dbdfc7317c616c
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-14 01:46:45 -0400

    configuracion ruteo
    configurado redireccion, y botones de add y edit

--- front-crud-dota-2-chile/src/app/app-routing.module.ts
+++ front-crud-dota-2-chile/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
 
-const routes: Routes = [];
+
+// Componentes
+const routes: Routes = [
+  { path: '', component: ListUsersComponent },
+  { path: '**',redirectTo: '', pathMatch: 'full' },
+  { path: 'add', component: AddEditUserComponent },
+  { path:'edit/:id', component: AddEditUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
